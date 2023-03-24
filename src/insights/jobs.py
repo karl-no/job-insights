864330711b7c3155893c1dc9ec07aa2f4aa471d1
karl-no <karl.publicidade@gmail.com>
2023-03-24 17:20:00 -0300

    feat: module to read csv files

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -1,22 +1,13 @@
+import csv
 from functools import lru_cache
 from typing import List, Dict
 
 
 @lru_cache
 def read(path: str) -> List[Dict]:
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    raise NotImplementedError
+    with open(path, encoding="utf-8") as file:
+        jobs_reader = csv.DictReader(file, delimiter=",", quotechar='"')
+        return list(jobs_reader)
 
 
 def get_unique_job_types(path: str) -> List[str]:
