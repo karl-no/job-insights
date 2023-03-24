581dfa1e029632064d51723afe044dc7ff7f173b
karl-no <karl.publicidade@gmail.com>
2023-03-24 17:59:31 -0300

    feat: module to get job types

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -11,21 +11,13 @@ def read(path: str) -> List[Dict]:
 
 
 def get_unique_job_types(path: str) -> List[str]:
-    """Checks all different job types and returns a list of them
+    jobs_reader = read(path)
+    job_type = set()
 
-    Must call `read`
+    for job in jobs_reader:
+        job_type.add(job["job_type"])
 
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique job types
-    """
-    raise NotImplementedError
+    return job_type
 
 
 def filter_by_job_type(jobs: List[Dict], job_type: str) -> List[Dict]:
