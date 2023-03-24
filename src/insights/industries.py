6536902b3ba1f56886b7a5a783c50c6e7f03d08f
karl-no <karl.publicidade@gmail.com>
2023-03-24 18:32:53 -0300

    feat: module to get unique industries

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,22 +1,16 @@
 from typing import List, Dict
+from src.insights.jobs import read
 
 
 def get_unique_industries(path: str) -> List[str]:
-    """Checks all different industries and returns a list of them
+    industries_reader = read(path)
+    industries = set()
 
-    Must call `read`
+    for industry in industries_reader:
+        if len(industry["industry"]) > 0:
+            industries.add(industry["industry"])
 
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    raise NotImplementedError
+    return industries
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
