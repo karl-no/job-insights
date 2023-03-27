f1d701abb50d7dc55c975523a9e38581f2e930a0
karl-no <karl.publicidade@gmail.com>
2023-03-27 15:02:48 -0300

    test: read_brazilian_file

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,15 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
+
+
+mock_dict_english = {
+    "title": "Maquinista",
+    "salary": "2000",
+    "type": "trainee",
+}
+
+
+path = "tests/mocks/brazilians_jobs.csv"
 
 
 def test_brazilian_jobs():
-    pass
+    assert read_brazilian_file(path)[0] == mock_dict_english
